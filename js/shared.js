558b1f12d3f05bd4c31e010767e7655eae1e9708
Sai-Akshith <saiakshith223@gmail.com>
2025-12-02 12:13:33 +0530

    Delete js/shared.js

--- js/shared.js
+++
@@ -1,8 +0,0 @@
-// js/shared.js
-function format(amount) {
-  if (isNaN(amount)) return "₹0";
-  return "₹" + amount.toLocaleString("en-IN", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  });
-}
