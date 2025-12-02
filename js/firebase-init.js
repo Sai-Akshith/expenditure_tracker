c27dd9ae097a36dcb9fc6036a2404b32c04122b2
Sai-Akshith <saiakshith223@gmail.com>
2025-12-02 12:08:49 +0530

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -2,13 +2,14 @@
 
 // TODO: replace this with your actual config from Firebase console
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "XXXXXXX",
-  appId: "1:XXXXXXX:web:XXXXXXXX"
-};
+    apiKey: "AIzaSyDK04VsSPPISd7KWFrPQ5r1VE4h3hfiuQA",
+    authDomain: "expenditure-tracker-5569c.firebaseapp.com",
+    projectId: "expenditure-tracker-5569c",
+    storageBucket: "expenditure-tracker-5569c.firebasestorage.app",
+    messagingSenderId: "946973275836",
+    appId: "1:946973275836:web:d388f6ff00eb76b4cf82b9",
+    measurementId: "G-LSYZKXVB0S"
+  };
 
 // Initialize Firebase (compat style)
 firebase.initializeApp(firebaseConfig);
