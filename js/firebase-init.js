e15e871357a1a2ab04511a44a3e1518ea7a8b53c
Sai-Akshith <saiakshith223@gmail.com>
2025-12-02 13:38:16 +0530

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -4,10 +4,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyDK04VsSPPISd7KWFrPQ5r1VE4h3hfiuQA",
   authDomain: "expenditure-tracker-5569c.firebaseapp.com",
   projectId: "expenditure-tracker-5569c",
-  storageBucket: "expenditure-tracker-5569c.firebasestorage.app"
+  storageBucket: "expenditure-tracker-5569c.appspot.com",
   messagingSenderId: "946973275836",
   appId: "1:946973275836:web:d388f6ff00eb76b4cf82b9"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();   // KEEP THIS ONLY HERE
